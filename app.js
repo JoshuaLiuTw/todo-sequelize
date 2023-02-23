41f1a5e4f97fba5100d090c4bc69845ff593a5ca
Joshua <j2h5068312@gmail.com>
2023-02-24 06:04:42 +0800

    add Seeder

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+
 const app = express()
 const PORT = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -13,4 +17,30 @@ app.get('/', (req, res) => {
 })
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
+})
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.create({ name, email, password })
+    .then(user => res.redirect('/'))
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
 })
