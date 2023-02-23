ff6f81c74bd5f1f88f40536da48054136764243b
Joshua <j2h5068312@gmail.com>
2023-02-24 06:52:12 +0800

    add login function

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const bcrypt = require('bcryptjs')
 const db = require('./models')
 const Todo = db.Todo
 const User = db.User
+const session = require('express-session')
+// 引用 passport，放在文件上方
+const passport = require('passport')
+
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
 
 const app = express()
 const PORT = 3000
@@ -12,6 +18,16 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
+
 app.get('/', (req, res) => {
   return Todo.findAll({
     raw: true,
@@ -32,9 +48,11 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+// 加入 middleware，驗證 reqest 登入狀態
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
