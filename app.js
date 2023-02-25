2f2ea60f63a711aeb4b685632e1e81f24448c7ca
Joshua <j2h5068312@gmail.com>
2023-02-25 12:30:32 +0800

    message partial

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
 const routes = require('./routes')
 
@@ -14,6 +15,8 @@ const usePassport = require('./config/passport')
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
@@ -33,6 +36,7 @@ app.use(session({
 
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
+app.use(flash())
 
 app.use(routes)
 
